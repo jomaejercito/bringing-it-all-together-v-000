978181245d7c9732a12b84dfc0377cf1383b9bf1
Joma Ejercito <jomaejercito@gmail.com>
2018-04-02 21:25:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -97,13 +97,8 @@ class Dog
   end
 
   def update
-    sql = <<-SQL
-      UPDATE dogs
-      SET name=?, breed=?
-      WHERE id=?
-      SQL
-
-      DB[:conn].execute(sql, self.name, self.breed, self.id)
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 
